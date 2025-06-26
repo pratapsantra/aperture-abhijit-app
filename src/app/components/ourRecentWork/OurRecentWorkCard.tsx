633ce4dfa31888f507d1c852933e7f39ad8c8918
pratapcs <79842865+pratapcs@users.noreply.github.com>
2025-06-26 17:47:38 +0530

    new updated

--- src/app/components/ourRecentWork/OurRecentWorkCard.tsx
+++ src/app/components/ourRecentWork/OurRecentWorkCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import Image from 'next/image';
 import './RecentWork.css'
 
