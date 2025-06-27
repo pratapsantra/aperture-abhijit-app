07ca983656c10dad046bcd2c3ff13e86fe22de80
pratapcs <79842865+pratapcs@users.noreply.github.com>
2025-06-27 21:08:23 +0530

    test 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       {/* <Carousel /> */}
       <OurServices/>
-      {/* <OurRecentWork /> */}
+      <OurRecentWork />
       {/* <HappyClient /> */}
       {/* <OurTeam /> */}
       <NatureOfPhotography />
