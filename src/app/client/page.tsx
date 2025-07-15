952a234ed91e78e879afbc5b1e22bc4ed46cb926
pratapsantra <pratapsantra18@gmail.com>
2025-07-15 21:13:54 +0530

    client speech updated

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,14 +1,24 @@
 'use client';
 import React from "react";
+import InsideCarousel from "../components/InsideCarousel/insideCarousel";
+import aboutCarouselData from '../JsonData/aboutCarousel.json';
+import AboutusContent from "../components/AboutusComponent/AboutusContent/AboutusContent";
+import AboutusOurServices from "../components/AboutusOurServices/AboutusOurServices";
+import OurProcessItem from "../components/OurProcess/OurProcess";
+import Videography from "../components/Videography/Videography";
+import WhyChooseUs from "../components/WhyChooseUs/WhyChooseUs";
+import HappyClientAll from "../components/HappyClientAll/HappyClientAll";
 
-
-export default function aboutus() {
+export default function client() {
     return (
         <>
-            <section className='section-container padding-null page-postion '> 
-                <div className='inner-container'>
-                    <h3 className="text-center">Client</h3>
-                </div>
+            <section className='section-container padding-null  '>  {/* //page-postion */}
+                    <InsideCarousel data={aboutCarouselData} />
+                    <HappyClientAll />
+                    {/* <WhyChooseUs /> */}
+                    {/* <Videography /> */}
+                    {/* <AboutusOurServices /> */}
+                    {/* <OurProcessItem /> */}
             </section>
         </>
     );
---
+++ src/app/components/HappyClientAll/HappyClientAll.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import "./HappyClientAll.css"
+import allHappyClient from '../../JsonData/allHappyClient.json'
+
+interface ClientReviewData {
+    id: number;
+    imageUrl: string;
+    description: string;
+    background: string;  // Background color (hex or any CSS color)
+    clientName?: string
+}
+function HappyClientAll() {
+    const [clientReview, setClientReview] = useState<ClientReviewData[]>(allHappyClient)
+
+
+
+    useEffect(() => {
+        const updatedClients = allHappyClient.map(client => ({
+            ...client,
+            background: getRandomColor()
+        }));
+
+        setClientReview(updatedClients);
+        // Optional: return cleanup function if necessary
+        return () => {
+
+        };
+    }, []);
+
+    const getRandomColor = () => {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    return (
+        <section className='happy-client-all-section'>
+            <div className='inner-container'>
+                <div className='heading-container'>
+                    <div className='heading-text-container'>
+                        <h3>Our client Speech</h3>
+                    </div>
+                </div>
+            </div>
+            <div className="happy-client-all-container">
+
+                {clientReview && clientReview.map((client, index) => (
+                    <div className="happy-container">
+
+                        <div className="card" style={{ ['--card-bg' as any]: client.background}}>
+                            <div className="imgBx">
+                                <img src={client.imageUrl} />
+                            </div>
+                            <div className="contentBx">
+                                <h2>{client.clientName}</h2>
+                                <div className="size">
+                                    <p>{client.description}</p>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                ))}
+
+            </div>
+        </section>
+
+    );
+}
+
+export default HappyClientAll;
