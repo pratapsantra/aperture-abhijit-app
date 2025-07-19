b0fd7b9affa2e61b16bc150eb2e4856da1206ad5
pratapsantra <pratapsantra18@gmail.com>
2025-07-19 16:53:29 +0530

    test mail uploaded

--- src/app/components/OurServices.tsx
+++ src/app/components/OurServices.tsx
@@ -1,13 +1,38 @@
 // app/about/page.tsx
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import nodemailer from 'nodemailer'
 
 import ProjectCard from '../components/ourServices/ProjectCard'
 import ourServices from '../JsonData/ourServices.json'
 import { Slide, AttentionSeeker, Zoom, Bounce, Flip, Hinge, JackInTheBox, Roll } from "react-awesome-reveal";
 
 export default function OurServicesPage() {
+  useEffect(() => {
+    const sendTestMail = async () => {
+      const res = await fetch('/.netlify/functions/contact', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          fname: 'Test',
+          phone_no: '1234567890',
+          email: 'youremail@gmail.com',
+          message: 'Test message',
+          eventDate: '2025-07-19',
+          eventTypeOption: 'Test Event',
+          location: 'Test Location',
+        }),
+      })
+
+      const data = await res.json()
+      console.log('Response:', data)
+    }
+
+    sendTestMail()
+  }, [])
+
+
   return (
     <section className='section-container'>
       <div className='inner-container'>
